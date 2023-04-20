f61bc177e4aa1a68cf47391c04673546cee7d840
Edgar Ramírez <edgar.ramirez.fuentes.dev@gmail.com>
2023-04-19 18:42:29 -0600

    Add an update in the d1000000 problem approach in python

--- d1000000/d1000000.py
+++ d1000000/d1000000.py
@@ -3,26 +3,20 @@ Name: d1000000.py
 Last update: 18/04/2023
 """
 
-from collections import defaultdict
+import heapq
 
-def get_used_dice(dice_bucket : dict, max_die_value : int) -> int:
+def get_used_dice(dice_bucket : list) -> int:
     used_dice = 0
     target_die = 1
-    current_die = 1
-
-    # Greedy algorithm, which takes all the possible dice to form a straight.
-    while (current_die <= max_die_value):
-        if (current_die >= target_die and dice_bucket[current_die] > 0):
-            while (current_die >= target_die and dice_bucket[current_die] > 0):
-                used_dice += 1
-                dice_bucket[current_die] -= 1
-                target_die += 1
+    # Greedy approach.
+    while len(dice_bucket) > 0:
+        current_die = heapq.heappop(dice_bucket)
+        if current_die >= target_die:
+            used_dice += 1
+            target_die += 1
         
-        current_die += 1
 
     return used_dice
-        
-
         
 
 if __name__ == '__main__':
@@ -30,17 +24,10 @@ if __name__ == '__main__':
 
     for testCase in range(testCases):
         dice = int(input())
-        # Keep track of the times a n sides die appears.
-        dice_bucket = defaultdict(int)
-        input_dice = map(int, input().split())
-        # Keep the die with the max sides
-        max_die_value = 0
+        # Keep track of the times a n sides die appears. 
+        input_dice = [int(x) for x in input().split(' ')]
+        # Using a heap to sort the input dice.
+        heapq.heapify(input_dice)
 
-        # Counting the dice and getting the die with the max sides.
-        for die in input_dice:
-            max_die_value = max(max_die_value, die)
-            dice_bucket[die] += 1
-
-        used_dice = get_used_dice(dice_bucket, max_die_value)
+        used_dice = get_used_dice(input_dice)
         print(f'Case #{testCase + 1}: {used_dice}')
-        
